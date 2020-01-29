6b67f669115594e3a1aee753fa43c2086a14825b
Felix Wieland <mail@felixwie.com>
2020-01-29 21:35:42 +0100

    fixed small bug with export syntax

--- helper/ocr.js
+++ helper/ocr.js
@@ -1,14 +1,16 @@
-module.exports = function getOCR(imagePath) {
-    return new Promise(function(success, nosuccess) {
-        const { spawn } = require('child_process')
-        const py = spawn('python', ['./ocr.py', imagePath])
-    
-        py.stdout.on('data', function(data) {
-            success(data)
+module.exports = {
+    getOCR: function (imagePath) {
+        return new Promise(function(success, nosuccess) {
+            const { spawn } = require('child_process')
+            const py = spawn('python', ['./ocr.py', imagePath])
+        
+            py.stdout.on('data', function(data) {
+                success(data)
+            })
+        
+            py.stderr.on('data', function(data) {
+                nosuccess(data)
+            })
         })
-    
-        py.stderr.on('data', function(data) {
-            nosuccess(data)
-        })
-    })
+    }
 }
