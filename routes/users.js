0556fd82202578296046d87fb79b47f5aa497774
Felix Wieland <mail@felixwie.com>
2020-02-03 21:16:13 +0100

    whole user login/signup functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+var user = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('user home');
-});
+router.route('/login')
+  .post(async (req, res) => {
+    try {
+      let result = await user.getAuthenticated(req.body.username, req.body.password)
+      req.session.user = result
+      req.session.isAuthenticated = true
+      res.redirect('/documents')
+    } catch (error) {
+      req.flash('success', `Following error occured - ${error}`)
+      res.redirect('/user/login')
+    }
+  })
+  .get(async (req, res) => {
+    if(req.session.isAuthenticated) {
+      res.redirect('/documents')
+    } else {
+      res.render('login')
+    }
+  })
 
-module.exports = router;
+router.route('/signup')
+  .get(async (req, res) => {
+    if(req.session.isAuthenticated) {
+      res.redirect('/documents')
+    } else {
+      res.render('signup')
+    }
+  })
+  .post(async (req, res) => {
+    let newUser = new user({
+      username: req.body.username,
+      password: req.body.password,
+      mail: req.body.email
+    })
+
+    await newUser.save()
+    req.session.user = newUser
+    req.session.isAuthenticated = true
+
+    res.redirect('/')
+  })
+
+router.get('/logout', async (req, res) => {
+  await req.session.destroy()
+  res.redirect('/')
+})
+
+module.exports = router
