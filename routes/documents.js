e0ea9fc2160a923d0d4cc2bc9148a368e5d18c5e
Felix Wieland <mail@felixwie.com>
2020-02-13 23:28:29 +0100

    helper function to delete all files in a s3 dir

--- routes/documents.js
+++ routes/documents.js
@@ -90,6 +90,29 @@ router.route('/delete/:fileid')
       res.redirect('/documents')
     }
   })
-  .put()
+// HELPER FUNC
+async function emptyS3Directory(bucket, dir) {
+  const listParams = {
+      Bucket: bucket,
+      Prefix: dir
+  }
+
+  const listedObjects = await s3.listObjectsV2(listParams).promise()
+
+  if (listedObjects.Contents.length === 0) return
+
+  const deleteParams = {
+      Bucket: bucket,
+      Delete: { Objects: [] }
+  }
+
+  listedObjects.Contents.forEach(({ Key }) => {
+      deleteParams.Delete.Objects.push({ Key })
+  })
+
+  await s3.deleteObjects(deleteParams).promise()
+
+  if (listedObjects.IsTruncated) await emptyS3Directory(bucket, dir)
+}
 
 module.exports = router
