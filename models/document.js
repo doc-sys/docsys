4406a0b9f9ff1f997c62d2ccf647eb259ab592ce
Felix Wieland <mail@felixwie.com>
2020-02-14 18:57:05 +0100

    reformatted and swapped virtual with lock prop

--- models/document.js
+++ models/document.js
@@ -3,59 +3,56 @@ let { getOCR } = require('../helper/ocr')
 let { getKeywords } = require('../helper/keywords')
 
 let document = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    created: {
-        type: Date,
-        default: Date.now()
-    },
-    dated: {
-        type: Date
-    },
-    content: {
-        type: String
-    },
-    keywords: {
-        type: [String],
-        minlength: 3
-    },
-    fileId: {
-        type: String,
-        unique: true
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: false
-    },
-    sharedWith: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        default: []
-    }],
-    lockedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        default: null
-    },
-    mime: {
-        type: String
-    },
-    extension: {
-        type: String
-    }
+	title: {
+		type: String,
+		required: true,
+	},
+	created: {
+		type: Date,
+		default: Date.now(),
+	},
+	dated: {
+		type: Date,
+	},
+	content: {
+		type: String,
+	},
+	keywords: {
+		type: [String],
+		minlength: 3,
+	},
+	fileId: {
+		type: String,
+		unique: true,
+	},
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: false,
+	},
+	sharedWith: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			default: [],
+		},
+	],
+	lockedBy: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		default: null,
+	},
+	locked: {
+		type: Boolean,
+		default: false,
+	},
+	mime: {
+		type: String,
+	},
+	extension: {
+		type: String,
+	},
 })
-
-document.virtual('locked')
-    .get(() => {
-        if(this.lockedBy == null) {
-            return false
-        } else {
-            return true
-        }
-    })
 
 /* document.pre('save', async function(next) {
     try {
