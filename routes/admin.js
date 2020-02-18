e17fbd8f8093a8c9c2ac3e9f61ecb2e264481513
Felix Wieland <mail@felixwie.com>
2020-02-18 08:39:31 +0100

    reworked

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,26 @@
 var express = require('express')
 var router = express.Router()
 
-const user  = require('../models/user')
-const doc   = require('../models/document')
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
-})
+const user = require('../models/user')
+const doc = require('../models/document')
 
 router.get('/session', (req, res) => {
-  res.send(req.session.user)
+	res.send(req.session.user)
 })
 
 router.get('/db_user', async (req, res) => {
-  res.send(await user.find())
+	res.send(await user.find())
 })
 
 router.get('/db_docs', async (req, res) => {
-  res.send(await doc.find())
+	res.send(await doc.find())
+})
+
+router.get('/clear', async (req, res) => {
+	await user.remove()
+	await doc.remove()
+
+	res.send('cleaned')
 })
 
 module.exports = router
