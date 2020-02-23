17c0617ac894ad6458b1f3c4e5ae60a089c18b31
Felix Wieland <mail@felixwie.com>
2020-02-23 11:33:19 +0100

    merged file editing queues into one file

--- queues/file_editing.js
+++ queues/file_editing.js
@@ -1,3 +1,29 @@
+// OCR QUEUE
+
+const Queue = require('bee-queue')
+const fs = require('fs')
+const mongoose = require('mongoose')
+
+require('dotenv').config({ path: '../.env' })
+
+const queue = new Queue('ocr', { isWorker: true })
+mongoose.connect(process.env.DB_PATH)
+
+let docs = require('../models/document')
+
+queue.process(async function(job, done) {
+	if (fs.existsSync('./files/' + job.data.filename)) {
+		// PROCESS FILE WITH OCR
+		console.log('PROCESS OCR')
+		await fs.unlink('./files/' + job.data.filename)
+	} else {
+		console.log('NOT EXIST')
+	}
+	return done(null, true)
+})
+
+// CONVERSION QUEUE
+
 /* const Queue = require('bee-queue')
 const fs = require('fs')
 const PDFImage = require('pdf-image').PDFImage
