adfe75689b6f0935b5cfd43dc1ae93245a2446ce
Felix Wieland <mail@felixwie.com>
2020-03-17 21:35:47 +0100

    making ready for heroku

--- routes/helper.js
+++ routes/helper.js
@@ -2,10 +2,11 @@ var express = require('express')
 var router = express.Router()
 
 const user = require('../models/user')
-const doc = require('../models/document')
 
 router.get('/autocomplete', async (req, res) => {
-	res.json(await user.find().select('username settings.displayName'))
+	res
+		.status(200)
+		.json(await user.find().select('username settings.displayName'))
 })
 
 module.exports = router
