c8f3e4a2b1f189e6b2e073bf1214b53d39519204
Felix Wieland <mail@felixwie.com>
2020-04-14 20:15:55 +0200

    File needs to have the actual body

--- src/storage/storagefile.ts
+++ src/storage/storagefile.ts
@@ -1,9 +1,11 @@
 class StorageFile {
     fileId: string;
     origin: string;
+    body: Buffer;
 
     constructor(fileId: string, origin: string, body: Buffer) {
         this.fileId = fileId;
         this.origin = origin;
+        this.body = body;
     }
 }
