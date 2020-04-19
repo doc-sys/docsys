f621c95ca572a30209c4ff9fb31a465232a4d3f2
Felix Wieland <mail@felixwie.com>
2020-04-19 16:16:52 +0200

    Update interface.ts
    small change to GET return

--- src/storage/adapters/interface.ts
+++ src/storage/adapters/interface.ts
@@ -3,6 +3,7 @@ import { Readable } from 'stream'
 
 export default interface StorageAdapter {
     add(stream: Readable): Promise<string>;
+    get(id: string): Promise<Buffer>;
     delete(id: string): Promise<void>;
     archive(id: string): Promise<void>;
 }
