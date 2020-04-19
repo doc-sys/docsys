4c31bf1d1040336e0491a357b462d1b2d0ff7aa1
Felix Wieland <mail@felixwie.com>
2020-04-19 17:05:08 +0200

    Update authenticate.ts
    adding helper for api token check

--- src/lib/helpers/authenticate.ts
+++ src/lib/helpers/authenticate.ts
@@ -1,5 +1,25 @@
+// TYPES
 import { Request, Response, NextFunction } from 'express';
 
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
+//DEPS
+import * as jwt from 'jsonwebtoken'
+var { ErrorHandler } = require('./error')
 
+export default async function authenticate(req: any, res: Response, next: NextFunction) {
+    try {
+
+        let token = req.query.token || req.headers.authorization!.split(' ')[1] || undefined
+
+        if (token === undefined || token === null) {
+            throw new ErrorHandler(402, 'API Token must either be set as header or query string')
+        }
+
+        let result = await jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = result
+
+        next()
+    } catch (error) {
+        next(error)
+    }
 }
