024ee96cc993ebeb0d4de16b9a8c917954adbb37
Felix Wieland <mail@felixwie.com>
2020-04-25 20:31:58 +0200

    index to ts
    rewrote index to ts and added health checks

---
+++ src/controller/index.controller.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response, NextFunction } from "express"
+import { ErrorHandler } from '../lib/helpers/error'
+
+import * as mongoose from 'mongoose'
+import * as redisStatus from 'redis-status'
+
+export const checkMongoDBHealth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.locals.health.mongoose = mongoose.STATES[mongoose.connection.readyState]
+    } catch (error) {
+        return next(new ErrorHandler(500, `Error checking MongoDB health: ${(error as Error).message}`))
+    }
+
+    next()
+}
+
+export const checkStorageHealth = async (req: Request, res: Response, next: NextFunction) => {
+    //no idea how to implement this
+}
+
+export const checkRedisHealth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let redisURI = new URL(process.env.REDIS_URI as string) || {}
+        let redis = redisStatus({
+            name: redisURI.pathname,
+            port: redisURI.port,
+            host: redisURI.hostname,
+            password: redisURI.password
+        })
+
+        redis.checkStatus((err) => {
+            err ? res.locals.healt.redis = err : res.locals.health.redis = 'healthy'
+        })
+    } catch (error) {
+        return next(new ErrorHandler(500, `Error checking Redis health: ${(error as Error).message}`))
+    }
+
+    next()
+}
---
+++ src/routes/index.route.ts
@@ -0,0 +1,18 @@
+var express = require('express')
+var router = express.Router()
+
+import { checkMongoDBHealth, checkRedisHealth } from '../controller/index.controller';
+
+/**
+ * @api {get} / Server health
+ * @apiName serverPingHealth
+ * @apiGroup Root
+ * @apiDescription Returns information on the server health
+ * @apiSuccess {Object} healt Server health information
+ * @apiError (500) InternalServerError Something went wron processing your request
+ */
+router.route('/').get([checkMongoDBHealth, checkRedisHealth], (req, res) => {
+	res.status(200).json(res.locals.health)
+})
+
+module.exports = router
